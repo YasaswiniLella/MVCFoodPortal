e07ec847e3232f2df12b6d9c1259f35a230fd74e
YasaswiniLella <yasaswini.lella@gmail.com>
2020-11-11 19:52:58 +0530

    Add project files.

---
+++ MVCFoodPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCFoodPortal.MvcApplication" Language="C#" %>
---
+++ MVCFoodPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCFoodPortal.Startup))]
+namespace MVCFoodPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
