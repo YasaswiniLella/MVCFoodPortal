b88d275864f9c54321acb9aa01ddde41b3aa6315
tejasri757 <tejasrikomanduri804@gmail.com>
2020-11-18 11:36:52 +0530

    placeorder

--- MVCFoodPortal/Controllers/HomeController.cs
+++ MVCFoodPortal/Controllers/HomeController.cs
@@ -35,6 +35,11 @@ namespace MVCFoodPortal.Controllers
             return View(food);
 
         }
+        public ActionResult PlaceOrder(int id)
+        {
+            var food = dbContext.Food.FirstOrDefault(x => x.Id == id);
+            return View(food);
+        }
 
     }
 
